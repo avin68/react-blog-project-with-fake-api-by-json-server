885fb673a674050470bf95838abe90c45e1ef80f
avin <esmaili_avin@yahoo.com>
2024-11-28 00:35:12 +0330

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import AddArticle from "./pages/addArticle/AddArticle";
+import Article from "./pages/article/Article";
+import EditArticle from "./pages/editArticle/EditArticle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/add-article" element={<AddArticle />} />
+        <Route path="/article/:articleId" element={<Article />} />
+        <Route path="/edit-article/:articleId" element={<EditArticle />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/about/About.js
@@ -0,0 +1,12 @@
+import MyNavbar from "../../components/navbar/MyNavbar";
+
+function About() {
+    return (
+        <>
+            <MyNavbar />
+            <h1>about Page</h1>
+        </>
+    );
+}
+
+export default About;
